838c7776a43d5da7ec0d470a8093bcc6fe229666
Ramon Rodrigues <ramon.rpa2018@gmail.com>
2024-06-19 09:47:46 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,149 @@
-import React, { useEffect, useState } from "react";
-import "./App.css"
+import { useState, useRef, useEffect, useCallback, useMemo } from "react";
+import "./App.css";
 
-function App() {
+const defaultConfig = {
+  time: 10000,
+  countLetters: 6,
+};
 
-  const [random, setRandom] = useState([]);
-  const [showGame, setShowGame] = useState(false)
-  let timer = null
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+const App = () => {
+  const lettersRef = useRef([]);
+
+  const [config, setConfig] = useState(defaultConfig);
+  const [letters, setLetters] = useState([]);
+  const [inGame, setInGame] = useState(false);
+  const [position, setPosition] = useState(0);
+
+  const intervalRef = useRef(null);
+
+  const generateLetters = () => {
+    return Array.from({ length: config.countLetters }, () => {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      return alphabet[randomIndex];
+    });
+  };
+
+  const startGame = () => {
+    setLetters(generateLetters());
+
+    setInGame(true);
+  };
+
+  const resetGame = () => {
+    setInGame(false);
+    setPosition(0);
+  };
+
+  const renderLetter = useCallback(
+    (letter, index) => {
+      return (
+        <span
+          key={letter}
+          ref={(el) => (lettersRef.current[index] = el)}
+          className={`letter ${
+            index === position
+              ? "letter--selected"
+              : index < position
+              ? "letter--correct"
+              : ""
+          }`}
+          onAnimationEnd={(event) => {
+            if (event.animationName === "pulseError")
+              event.target.classList.remove("letter--error");
+          }}
+        >
+          {letter.toUpperCase()}
+        </span>
+      );
+    },
+    [position]
+  );
+
+  const renderLetters = useMemo(() => {
+    return letters.map(renderLetter);
+  }, [letters, renderLetter]);
+
+  const handleKeyDown = useCallback(
+    (e) => {
+      if (e.key === letters[position]) {
+        setPosition((prev) => prev + 1);
+      } else {
+        const letterRef = lettersRef.current[position];
+        letterRef.classList.remove("letter--error");
+        letterRef.classList.add("letter--error");
+      }
+    },
+    [letters, position]
+  );
 
   useEffect(() => {
-    timer = setTimeout(() => {
-      setShowGame(false)
-    }, 5000);
-
-    return () => clearTimeout(timer)
-
-  }, [showGame])
-
-  useEffect(()=>{
-    window.addEventListener("keyup", compareF);
-
-    return ()=>{
-      window.removeEventListener("keyup", compareF)
+    if (position === letters.length && inGame) {
+      alert("Você venceu!");
+      resetGame();
     }
-  }, [compareF])
+  }, [inGame, letters.length, position]);
 
-  let globalArray = []
-  let posicao = 0
+  useEffect(() => {
+    if (inGame)
+      intervalRef.current = setInterval(() => {
+        alert("Tempo esgotado!");
+        resetGame();
+      }, config.time);
 
-  function genRandom() {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [inGame, config.time]);
 
-    const randomIt = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-    const splitRandomIt = randomIt.split(",");
+  useEffect(() => {
+    if (inGame) document.addEventListener("keydown", handleKeyDown);
 
-    for (let i = 0; i < 6; i++) {
-
-      const randomPosition = splitRandomIt[Math.floor(Math.random() * 25)];
-      globalArray.push(randomPosition);
-    }
-    setRandom(globalArray)
-    setShowGame(true)
-  }
-
-  function compareF(e) {
-
-    const keyPressed = e.key.toUpperCase();
-
-    console.log(random);
-    console.log(keyPressed);
-    // usando array do state
-    if (random[posicao] == keyPressed) {
-      posicao++
-      console.log(posicao)
-    }
-
-    if (random[5] == keyPressed) {
-      alert("PARABÉNS!")
-      setShowGame(false)
-      clearTimeout(timer)
-    }
-  }
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown, inGame]);
 
   return (
-    <>
-      <div className="container" >
-        {/* <p>{showGame && random.join(" | ")}</p> */}
-
-        <div>{showGame && random.map((item, idx)=>{
-          
-        return (<div key={idx} className={posicao > idx ? "posicao": ""}>{item}</div>)
-        
-        })}</div>
-     
-        <div className={showGame ? "timer" : ""}></div>
-
-        <button onClick={genRandom} className={showGame? "btn": ""}>Iniciar</button>
-
-      </div>
-    </>
-  )
-
-}
-export default App
+    <div className='container'>
+      {inGame ? (
+        <div className='wrapper'>
+          <div className='letters'>{renderLetters}</div>
+          <div className='timer'>
+            <div
+              className='timer__progress'
+              style={{ animationDuration: `${config.time}ms` }}
+            ></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h1>Configurar</h1>
+          <label className='input-group'>
+            Tempo:{" "}
+            <input
+              type='number'
+              value={config.time}
+              onChange={(e) => setConfig({ ...config, time: e.target.value })}
+            />
+          </label>
+          <label className='input-group'>
+            Quantidade de letras:{" "}
+            <input
+              type='number'
+              value={config.countLetters}
+              onChange={(e) =>
+                setConfig({ ...config, countLetters: e.target.value })
+              }
+            />
+          </label>
+          <button className='btn' onClick={startGame}>
+            Iniciar
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+export default App;
